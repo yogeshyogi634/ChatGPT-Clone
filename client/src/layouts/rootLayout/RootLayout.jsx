7084bbedf7ad62e316b18ea19e83aeea208b2074
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-08-08 16:07:49 +0530

    files added

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,12 +1,13 @@
 import { Link, Outlet } from "react-router-dom";
 import "./rootLayout.scss";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
 import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,25 +15,29 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <div className="rootLayout">
-        <header>
-          <Link to="/" className="logo">
-            <img src="/logo.png" alt="" />
-            <span>IndiaBot</span>
-          </Link>
-          <div className="user">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/" className="logo">
+              <img src="/logo.png" alt="" />
+              <span>IndiaBot</span>
+            </Link>
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
